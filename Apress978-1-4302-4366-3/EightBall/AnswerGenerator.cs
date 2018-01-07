f739b942ce61c3520e30ac54675bec717a5c4d94
blackheart100 <1290823140@qq.com>
2018-01-07 14:53:48 +0800

    41

---
+++ Apress978-1-4302-4366-3/EightBall/AnswerGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EightBall
+{
+    public  class AnswerGenerator
+    {
+        public string GetRandomAnswer(string question)
+        {
+            return string.Format("你问我的问题是{0}",question);
+        }
+    }
+}
